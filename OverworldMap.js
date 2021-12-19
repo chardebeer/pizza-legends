ca65a6a72442c8880d31e6d211a77d8d313035f1
Charlize de Beer <chardebeer94@gmail.com>
2021-12-19 18:28:47 +0200

    Add more maps and character moves

--- OverworldMap.js
+++ OverworldMap.js
@@ -22,6 +22,44 @@ class OverworldMap {
 }
 
 window.OverworldMap = {
-    DemoRoom: "",
-    Kitchen: "",
+    DemoRoom: {
+        lowerSrc: "/images/maps/DemoLower.png",
+        upperSrc: "/images/maps/DemoUpper.png",
+        gameObjects: {
+            hero: new GameObject({
+                x: 5,
+                y: 6,
+            }),
+            npc1: new GameObject({
+                x: 7,
+                y: 9,
+                src: "/images/characters/people/npc1.png"
+            })
+
+        }
+
+
+    },
+    Kitchen: {
+        lowerSrc: "/images/maps/KitchenLower.png",
+        upperSrc: "/images/maps/KitchenUpper.png",
+        gameObjects: {
+            hero: new GameObject({
+                x: 3,
+                y: 5,
+            }),
+            npcA: new GameObject({
+                x: 9,
+                y: 7,
+                src: "/images/characters/people/npc2.png"
+            }),
+            npcB: new GameObject({
+                x: 10,
+                y: 4,
+                src: "/images/characters/people/npc3.png"
+            })
+        }
+
+
+    },
 }
